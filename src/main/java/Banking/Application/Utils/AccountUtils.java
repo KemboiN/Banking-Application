f7b345ffd09bb842604a5c59d2d81f05037d3483
Kemboi Nehemiah <nehemiahkimutai32@gmail.com>
2024-10-14 04:31:33 +0300

    Utills edit

--- src/main/java/Banking/Application/Utils/AccountUtils.java
+++ src/main/java/Banking/Application/Utils/AccountUtils.java
@@ -5,14 +5,10 @@ import java.time.Year;
 public class AccountUtils {
     public static final String ACCOUNT_CREATED__CODE="002";
     public static final String ACCOUNT_CREATED_MESSAGE="YOUR ACCOUNT WAS CREATED SUCCESSFULLY";
-    public static final String ACCOUNT_NOT_EXIST_CODE ="003";
-    public static final String ACCOUNT_NOT_EXIST_MESSAGE ="THE USER WITH PROVIDED ACCOUNT NUMBER DOES NOT EXIST";
     public static final String ACCOUNT_FOUND_CODE ="004";
     public static final String ACCOUNT_FOUND_MESSAGE ="THE USER WITH ACCOUNT NUMBER FOUND!!!!";
     public static final String ACCOUNT_CREDITED_SUCCESS_CODE ="005";
     public static final String ACCOUNT_CREDITED_SUCCESS_MESSAGE ="ACCOUNT CREDITED SUCCESSFULLY";
-    public static final String INSUFFICIENT_BALANCE_CODE ="006";
-    public static final String INSUFFICIENT_BALANCE_MESSAGE ="SORRY, THE AVAILABLE BALANCE IS INSUFFICIENT";
     public static final String ACCOUNT_DEBITED_SUCCESS_CODE ="007";
     public static final String ACCOUNT_DEBITED_SUCCESS_MESSAGE ="ACCOUNT DEBITED SUCCESSFULLY";
     public static final String TRANSFER_SUCCESS_CODE ="008";
